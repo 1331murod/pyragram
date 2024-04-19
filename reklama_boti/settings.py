401af1796632d62b661190252bcade8c131aa517
murod <m909201331@gmail.com>
2024-04-19 17:53:33 +0500

    salom

--- reklama_boti/settings.py
+++ reklama_boti/settings.py
@@ -128,7 +128,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+
+]
+STATIC_ROOT = BASE_DIR / "staticfiles/"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
